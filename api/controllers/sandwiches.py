eda45077365973aaccb318edaa87f904fe0d78a0
thanh <ttran112@charlotte.edu>
2026-03-27 23:02:52 -0400

    added create function

--- api/controllers/sandwiches.py
+++ api/controllers/sandwiches.py
@@ -1,3 +1,13 @@
 from sqlalchemy.orm import Session
 from fastapi import status, Response
 from ..models import models
+
+def create(db: Session, sandwich):
+    db_sandwich = models.Sandwich(
+        sandwich_name=sandwich.sandwich_name,
+        price=sandwich.price
+    )
+    db.add(db_sandwich)
+    db.commit()
+    db.refresh(db_sandwich)
+    return db_sandwich
