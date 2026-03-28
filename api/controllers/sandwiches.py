3815a10437c2ee8edbb9641a12895fff84acc87b
thanh <ttran112@charlotte.edu>
2026-03-27 23:04:05 -0400

    added update method

--- api/controllers/sandwiches.py
+++ api/controllers/sandwiches.py
@@ -11,3 +11,10 @@ def create(db: Session, sandwich):
     db.commit()
     db.refresh(db_sandwich)
     return db_sandwich
+
+def update(db: Session, sandwich_id, sandwich):
+    db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
+    update_data = sandwich.model_dump(exclude_unset=True)
+    db_sandwich.update(update_data, synchronize_session=False)
+    db.commit()
+    return db_sandwich.first()
